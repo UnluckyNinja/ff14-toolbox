9be8936e8e4c7cf01cd70e8d7aec7f094b83894a
UnluckyNinja <UnluckyNinja@users.noreply.github.com>
2023-05-29 15:33:03 +0800

    fix: settings not correctly saved

--- composables/settings.ts
+++ composables/settings.ts
@@ -1,18 +1,14 @@
 export function useSettings() {
-  const state = useState('item-search:settings', () => {
-    const defaultSettings = {
-      showOptions: true,
-      onlyHQ: true,
-      selectedRegion: '',
-      selectedDataCenter: '',
-      selectedServer: 0 as string | number,
-      numberPerPage: 20,
-    }
+  const defaultSettings = {
+    showOptions: true,
+    onlyHQ: false,
+    selectedRegion: '',
+    selectedDataCenter: '',
+    selectedServer: '',
+    numberPerPage: 20,
+  }
 
-    const settings = useLocalStorage('item-search:settings', defaultSettings, { mergeDefaults: true })
+  const settings = useLocalStorage('item-search:settings', defaultSettings, { mergeDefaults: true })
 
-    return settings
-  })
-
-  return state
+  return settings
 }
