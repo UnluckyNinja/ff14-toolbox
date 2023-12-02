6d6fa7d1c0c5165252fc274cd4561365c09d0213
UnluckyNinja <UnluckyNinja@users.noreply.github.com>
2023-12-02 23:37:34 +0800

    feat: pwa support

--- app.vue
+++ app.vue
@@ -9,10 +9,32 @@ if (colorMode.preference === 'system')
 function toggle() {
   colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
 }
+
+const { $pwa } = useNuxtApp()
+
+const toast = useToast()
+
+onMounted(() => {
+  if ($pwa?.offlineReady) {
+    toast.add({
+      id: 'update_pwa',
+      title: '网站内容有变化，点击刷新按钮以更新',
+      timeout: 0,
+      actions: [{
+        label: '刷新',
+        click: () => {
+          $pwa.updateServiceWorker()
+        }
+      }]
+    })
+  }
+})
 </script>
 
 <template>
   <div>
+    <NuxtPwaManifest />
+    <NuxtLoadingIndicator />
     <nav>
       <UContainer class="mt-4 flex items-center border rounded-lg bg-gray-200 py-2 shadow dark:bg-gray-800">
         <NuxtLink to="/">
@@ -21,12 +43,14 @@ function toggle() {
         <h1 class="mx-2 inline">
           {{ route.meta.title }}
         </h1>
-        <UButton class="ml-auto" :icon="colorMode.value === 'dark' ? 'i-heroicons-moon' : 'i-heroicons-sun' " @click="toggle" />
+        <UButton class="ml-auto" :icon="colorMode.value === 'dark' ? 'i-heroicons-moon' : 'i-heroicons-sun'"
+          @click="toggle" />
       </UContainer>
     </nav>
     <NuxtPage />
     <footer class="mx-auto mt-10 flex justify-center border-t py-10 text-2xl container">
-      <UButton size="xl" variant="link" color="white" to="https://github.com/UnluckyNinja/ff14-toolbox" target="_blank" icon="i-carbon-logo-github" />
+      <UButton size="xl" variant="link" color="white" to="https://github.com/UnluckyNinja/ff14-toolbox" target="_blank"
+        icon="i-carbon-logo-github" />
     </footer>
     <UNotifications class="sm:w-96" />
   </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
   },
   ssr: false,
   modules: [
+    '@vite-pwa/nuxt',
     '@nuxt/devtools',
     '@vueuse/nuxt',
     '@unocss/nuxt',
@@ -25,5 +26,47 @@ export default defineNuxtConfig({
 
   ui: {
     icons: ['carbon', 'mdi'],
+  },
+  
+  pwa: {
+    manifest: {
+      name: 'FF14 速查',
+      short_name: 'FF14速查',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
+    },
+    workbox: {
+      globPatterns: [
+        '**/*.{js,css,html,png,svg,ico}',
+        '_nuxt\/*.csv',
+        // '_nuxt\/*.wasm' // too large, "Configure maximumFileSizeToCacheInBytes to change this limit."
+      ],
+    },
+    client: {
+      installPrompt: true,
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallbackAllowlist: [/^\/$/],
+      type: 'module',
+    },
   },
 })
